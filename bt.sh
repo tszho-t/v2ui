3490267a515463aa23f92cfbdaddf7bd6d60358b
tszho-t <70687638+tszho-t@users.noreply.github.com>
2022-01-03 18:19:26 +0800

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -10,6 +10,3 @@ sed -i "/#检查是否需要升级/{s/#检查是否需要升级/return public.re
 sed -i "/update6.sh/{s/os.system/#os.system/g}" /www/server/panel/task.py
 sed -i "/update6.sh/{s/os.system/#os.system/g}" /www/server/panel/tools.py
 sed -i "/retry_count/{s/3/300/g}" /www/server/panel/class/acme_v2.py
-service firewalld stop
-chkconfig firewalld off
-
