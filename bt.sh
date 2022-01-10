c731ad8323a6da58589f9e88281ba38222baaeff
tszho-t <70687638+tszho-t@users.noreply.github.com>
2022-01-10 18:55:57 +0800

    Update bt.sh

--- bt.sh
+++ bt.sh
@@ -7,6 +7,6 @@ rm -f /www/server/panel/data/bind.pl
 rm -f LinuxPanel-7.7.0.zip
 rm -rf ./panel
 sed -i "/#检查是否需要升级/{s/#检查是否需要升级/return public.returnMsg(False,updateInfo)/g}" /www/server/panel/class/ajax.py
-sed -i "/update6.sh/{s/os.system/#os.system/g}" /www/server/panel/task.py
+#sed -i "/update6.sh/{s/os.system/#os.system/g}" /www/server/panel/task.py
 #sed -i "/update6.sh/{s/os.system/#os.system/g}" /www/server/panel/tools.py
 sed -i "/retry_count/{s/3/300/g}" /www/server/panel/class/acme_v2.py
