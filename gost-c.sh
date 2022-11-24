bd3fa91d169e79885874cdf4d35f0cf08bd08eba
tszho-t <70687638+tszho-t@users.noreply.github.com>
2022-11-24 12:36:45 +0800

    Add files via upload

---
+++ gost-c.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+wget https://github.com/ginuerzh/gost/releases/download/v2.11.4/gost-linux-amd64-2.11.4.gz
+tar zxvf gost-linux-amd64-2.11.4.gz
+chmod +x gost-linux-amd64
+mv gost-linux-amd64 /usr/bin/gost
+wget https://raw.githubusercontent.com/tszho-t/v2ui/master/gost-eve.service
+mv gost-eve.service /etc/systemd/system/gost-eve.service
+
+systemctl enable gost-eve.service
+systemctl start gost-eve.service
+
