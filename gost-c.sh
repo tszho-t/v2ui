1139814349c6d2caaf2d17d06a3b26317d57ff53
tszho-t <70687638+tszho-t@users.noreply.github.com>
2022-11-24 12:45:41 +0800

    Update gost-c.sh

--- gost-c.sh
+++ gost-c.sh
@@ -2,8 +2,8 @@
 
 wget https://github.com/ginuerzh/gost/releases/download/v2.11.4/gost-linux-amd64-2.11.4.gz
 gzip -d gost-linux-amd64-2.11.4.gz
-chmod +x gost-linux-amd64
-mv gost-linux-amd64 /usr/bin/gost
+chmod +x gost-linux-amd64-2.11.4
+mv gost-linux-amd64-2.11.4 /usr/bin/gost
 wget https://raw.githubusercontent.com/tszho-t/v2ui/master/gost-eve.service
 mv gost-eve.service /etc/systemd/system/gost-eve.service
 
